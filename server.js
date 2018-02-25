31cfb523fc2f6d82256a7740d3100cb9268386aa
David Jobe <daavid.jobe@gmail.com>
2018-02-25 16:26:15 +0100

    config for Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { resolve } = require('path')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.all('*', (req, res) => {
+  res.sendFile(resolve(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log('server error')
+  } else {
+    console.log(`server listening on port: ${PORT}`)
+  }
+})
